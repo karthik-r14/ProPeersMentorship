b68b6465b409d9d2ea211a65979b642d6cd080c6
Karthik Ramachandran <karthikr1493@gmail.com>
2025-05-27 15:34:24 +0530

    Create Foreground service.

--- app/src/main/java/com/mobileassistant/propeersmentorship/MainActivity.kt
+++ app/src/main/java/com/mobileassistant/propeersmentorship/MainActivity.kt
@@ -1,29 +1,60 @@
 package com.mobileassistant.propeersmentorship
 
+import android.Manifest
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.mobileassistant.propeersmentorship.ui.theme.ProPeersMentorshipTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
+        }
+
         enableEdgeToEdge()
         setContent {
             ProPeersMentorshipTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+                        Intent(applicationContext, RunningService::class.java).also {
+                            it.action = RunningService.Actions.START.toString()
+                            startService(it)
+                        }
+                    }) {
+                        Text(text = "Start run")
+                    }
+
+                    Button(onClick = {
+                        Intent(applicationContext, RunningService::class.java).also {
+                            it.action = RunningService.Actions.STOP.toString()
+                            startService(it)
+                        }
+                    }) {
+                        Text(text = "Stop run")
+                    }
                 }
             }
         }
