8e79067ba291c21e355d38abe527d13e4d77f0e4
Karthik Ramachandran <karthikr1493@gmail.com>
2025-06-07 15:29:27 +0530

    Create ThirdActivity with launchmode as SingleTask.

--- app/src/main/java/com/mobileassistant/propeersmentorship/launch_mode_demo/ThirdActivity.kt
+++ app/src/main/java/com/mobileassistant/propeersmentorship/launch_mode_demo/ThirdActivity.kt
@@ -1,52 +1,29 @@
 package com.mobileassistant.propeersmentorship.launch_mode_demo
 
-import android.app.ComponentCaller
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mobileassistant.propeersmentorship.R
 
-class SecondActivity : AppCompatActivity() {
+class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.activity_third)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         findViewById<Button>(R.id.launch_second_activity).setOnClickListener {
             Intent(applicationContext, SecondActivity::class.java).also {
                 startActivity(it)
             }
         }
-
-        findViewById<Button>(R.id.launch_third_activity).setOnClickListener {
-            Intent(applicationContext, ThirdActivity::class.java).also {
-                startActivity(it)
-            }
-        }
-
-        Toast.makeText(this,
-            getString(R.string.second_activity_on_create_msg), Toast.LENGTH_SHORT).show()
-
-    }
-
-    override fun onNewIntent(intent: Intent, caller: ComponentCaller) {
-        super.onNewIntent(intent, caller)
-        Toast.makeText(this,
-            getString(R.string.second_activity_on_newintent_msg), Toast.LENGTH_SHORT).show()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Toast.makeText(this,
-            getString(R.string.second_activity_on_newintent_msg), Toast.LENGTH_SHORT).show()
     }
 }
